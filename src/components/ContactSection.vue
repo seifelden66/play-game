ea0cb1af4de2c79458c049b031621feb4c2d3c7e
seifelden66 <seifelden66@gmail.com>
2025-02-17 22:07:20 +0200

    fixed contatc

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -82,7 +82,7 @@ export default {
   z-index: 999999999;
   max-width: 1200px;
   margin: 0 auto;
-  padding-top: 300px;
+  padding-top: 220px;
   align-items: baseline;
   display: flex;
   gap: 4rem;
