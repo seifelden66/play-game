a48fc815e0659a1e374c3bc6263e5e58e2845c4d
seifelden66 <31993069+seifelden66@users.noreply.github.com>
2025-02-23 10:54:25 +0200

    Update App.vue styles

--- src/App.vue
+++ src/App.vue
@@ -376,7 +376,8 @@ section.gameplan {
 
   /* Increase padding as desired */
   /* padding: 0.5rem; */
-  width: 290px;
+  width: 300px;
+  padding:2px;
   height: 70px;
   /* Apply a 15px border radius to the active item */
   border-radius: 15px;
